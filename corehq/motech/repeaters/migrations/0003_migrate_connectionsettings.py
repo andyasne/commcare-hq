8acc9467d0e4c67e821a2f32af111b19052a4a3b
Norman Hooper <nhooper@dimagi.com>
2021-01-08 01:11:32 +0200

    Migration to create RepeaterStubs

--- corehq/motech/repeaters/migrations/0003_migrate_connectionsettings.py
+++ corehq/motech/repeaters/migrations/0003_migrate_connectionsettings.py
@@ -1,19 +1,12 @@
 from django.db import migrations
 
-from corehq.motech.repeaters.models import Repeater
+from corehq.motech.repeaters.dbaccessors import iter_repeaters
 
 
 def _migrate_to_connectionsettings(apps, schema_editor):
     for repeater in iter_repeaters():
         if not repeater.connection_settings_id:
             repeater.create_connection_settings()
-
-
-def iter_repeaters():
-    for result in Repeater.get_db().view('repeaters/repeaters',
-                                         reduce=False,
-                                         include_docs=True).all():
-        yield Repeater.wrap(result['doc'])
 
 
 class Migration(migrations.Migration):
